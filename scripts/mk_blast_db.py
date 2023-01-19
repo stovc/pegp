fcdf18e02aa7fde448fda1a646e29bfaacd952fb
sovchinn <stepan.ovchinnikov@ist.ac.at>
2023-01-19 14:57:17 +0100

    mk_blast_db.py docstring

--- scripts/mk_blast_db.py
+++ scripts/mk_blast_db.py
@@ -1,6 +1,55 @@
-"""Make BLAST database input and annotation files.
+"""Make homology search database input and annotation files.
 
-- Step 01
+Iterate all Genebank genomes (.gbk) in the input directory in order to
+prepare
+- an _input file for construction of the homology search database_
+- _associated annotation files_
+
+For each genome, iterate all *sequence features* and make
+- 'proteins.fasta' with translations of protein coding genes
+- 'annotation.csv' with _annotations_
+- 'upstream.csv' with 200 bp upstream nucleotide sequences
+- 'downstream.csv' with 200 bp downstream nucleotide sequences
+- 'sequence.csv' with the nucleotide sequence of the genes
+- 'translation.csv' with translations protein coding genes
+where each record is preceded by an ID
+that is consistent across the files
+
+_Annotations_ are in the annotation.csv file and include:
+- Identity
+    - ID (generated for the created database)
+    - locus tag
+    - assembly
+    - accession
+- Position in the genome
+    - replicon (identity of the chromosome or the plasmid)
+    - start
+    - stop
+    - strand
+- Taxonomy information
+    - taxid (NCBI taxonomy ID)
+- Gene annotation
+    - feature type (..., ..., ...)
+    - gene
+    - product
+    - protein_id (should be rather in the annotation section)
+
+
+TODO: `taxids.txt` and  `organism_tree.nwk` should be generated in this script rather than in separate ones
+TODO: Extract *genome size* from the parsed genomes to `annotation.csv`
+TODO: Extract *replicon size* from the parsed genomes to `annotation.csv`
+note - genome size is a sum of the chromosomes; replicon size - size of the particular replicon (chromosome or plasmid)
+TODO: Extract GC content of the genome
+TODO: Extract GC content of the gene
+TODO: check if the genome context works properly regarding the circular genome
+
+
+# Features for later
+- consider deleting map_output
+- consider using locus tag as an ID
+- extract positions of the origin and terminus
+- Implement preparation of HMM database construction input
+- Psi-BLAST DB construction input? if i) we need it and ii) it is different from BLASTP
 """
 
 import os, sys
