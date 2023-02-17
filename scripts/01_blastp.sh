34bead59a7699be449a8e6f1f00ace71c481326a
stovc <stovch@gmail.com>
2023-02-17 11:46:53 +0100

    logging of exit

--- scripts/01_blastp.sh
+++ scripts/01_blastp.sh
@@ -6,3 +6,6 @@ echo "projects/$1/blastp.xml"
 
 echo 1 started >> projects/$1/exit_log.txt
 blastp -query projects/$1/input.faa -db databases/$2/protein.faa -out projects/$1/blastp.xml -outfmt 5 -max_target_seqs 100000
+
+# write step number and exit status into the exit log
+echo 1 $? >> projects/$1/exit_log.txt
