0eed42a3acf2defc2604c4d00f43b549e53c2c12
stovc <stovch@gmail.com>
2023-02-28 13:36:02 +0100

    dummy bash script for step 13

--- scripts/13_prottest.sh
+++ scripts/13_prottest.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+# this step is reserved for testing which substitution model suits
+# the alignment better for maximum likelihood inference
+# currently does nothing
 
-# WIP
+# log step number and started status
+echo 13 started >> projects/$1/exit_log.txt
+
+echo this step currently does nothing
+
+# write step number and exit status to the exit log
+echo 13 $? >> projects/$1/exit_log.txt
