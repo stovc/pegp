a37c57aa15a1fbc0d0a96e78d243734bc89e7670
stovc <stovch@gmail.com>
2023-02-28 13:18:50 +0100

    log errors

--- scripts/07_cluster_dict.py
+++ scripts/07_cluster_dict.py
@@ -17,48 +17,63 @@ import pandas as pd
 from pathlib import Path
 from Bio import SeqIO
 import subprocess
+import traceback
 
 if __name__ == '__main__':
-    # script argument - project
-    project = sys.argv[1]
+    try:
+        # script argument - project
+        project = sys.argv[1]
 
-    # construct input and output paths
-    in_clstr_path = Path('projects') / project / 'clustered90.faa.clstr'
+        # construct input and output paths
+        in_clstr_path = Path('projects') / project / 'clustered90.faa.clstr'
 
-    out_df_path = Path('projects') / project / 'cluster_dict.csv'
+        out_df_path = Path('projects') / project / 'cluster_dict.csv'
 
-    # logging to exit log
-    exitlog_path = Path('projects') / project / 'exit_log.txt'
+        # logging to exit log
+        exitlog_path = Path('projects') / project / 'exit_log.txt'
 
-    with open(exitlog_path, 'a') as outfile:
-        subprocess.run(["echo", '7 started'], stdout=outfile)
+        with open(exitlog_path, 'a') as outfile:
+            subprocess.run(["echo", '7 started'], stdout=outfile)
 
-    # open data
-    clstr = open(in_clstr_path, 'r')
-    out = open(out_df_path, 'w')
+        # open data
+        clstr = open(in_clstr_path, 'r')
+        out = open(out_df_path, 'w')
 
-    representative = ''
-    species_set = ''
-    species = []
+        representative = ''
+        species_set = ''
+        species = []
 
     d = {}
 
-    clstr.readline()
+        clstr.readline()
 
-    out.write('id,representative\n')
+        out.write('id,representative\n')
 
-    for line in clstr:
-        print(line)
-        if '*' in line:
-            representative = line.split(' ')[-2][1:-3]       # e.g., '5\t191aa, >AB000MSVVJ... at 99.48%' -> 'AB000MSVVJ'
-        if '%' in line:
-            spec = line.split(' ')[-3][1:-3]               # e.g., '3\t191aa, >AB000MT09Q... at 99.48%' -> AB000MT09Q
-            species.append(spec)
-        if 'Cluster' in line:
-            out.write(f'{representative},{representative}\n')
-            for sp in species:
-                out.write(f'{sp},{representative}\n')
-            species = []
+        for line in clstr:
+            print(line)
+            if '*' in line:
+                representative = line.split(' ')[-2][1:-3]       # e.g., '5\t191aa, >AB000MSVVJ... at 99.48%' -> 'AB000MSVVJ'
+            if '%' in line:
+                spec = line.split(' ')[-3][1:-3]               # e.g., '3\t191aa, >AB000MT09Q... at 99.48%' -> AB000MT09Q
+                species.append(spec)
+            if 'Cluster' in line:
+                out.write(f'{representative},{representative}\n')
+                for sp in species:
+                    out.write(f'{sp},{representative}\n')
+                species = []
 
-    out.close()
-    clstr.close()
+        out.close()
+        clstr.close()
+    except Exception as e:
+        ecx_type = str(type(e))
+
+        with open(exitlog_path, 'a') as outfile:
+            subprocess.run(["echo", '7 ' + ecx_type], stdout=outfile)
+
+        with open('log.txt', 'a') as outfile:
+            traceback.print_exc(file=outfile)
+
+    else:
+        # print exin code 0 to the exit log
+        with open(exitlog_path, 'a') as outfile:
+            subprocess.run(["echo", '7 0'], stdout=outfile)
