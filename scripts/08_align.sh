52b363116d3844e3d8482b264948582e8a57b97e
stovc <stovch@gmail.com>
2023-02-28 13:23:27 +0100

    added bash script for alignment

--- scripts/08_align.sh
+++ scripts/08_align.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+# align clustered and genome-consistent hits by MAFFT in the L-ins-i mode
 
-# WIP
+# args
+PROJECT=$1
+
+# log step number and started status
+echo 8 started >> projects/"$PROJECT"/exit_log.txt
+
+# align
+linsi --thread -1 projects/"$PROJECT"/clustered90-gc.faa > projects/"$PROJECT"/aligned.fa
+
+# write step number and exit status to the exit log
+echo 8 $? >> projects/"$PROJECT"/exit_log.txt
