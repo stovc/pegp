fe21f3d8a310b52a16a203326d7bcc63b8fec24b
stovc <stovch@gmail.com>
2023-02-28 13:18:20 +0100

    log errors

--- scripts/06_mk_genome_consistent.py
+++ scripts/06_mk_genome_consistent.py
@@ -13,40 +13,55 @@ import pandas as pd
 from pathlib import Path
 from Bio import SeqIO
 import subprocess
+import traceback
 
 if __name__ == '__main__':
-    # script argument - project
-    project = sys.argv[1]
+    try:
+        # script argument - project
+        project = sys.argv[1]
 
-    # construct input and output paths
-    in_faa_path = Path('projects') / project / 'clustered90.faa'
-    in_unclustered_faa_path = Path('projects') / project / 'filtered_hits.faa'
-    in_df_path = Path('projects') / project / 'filtered_hits.csv'
+        # construct input and output paths
+        in_faa_path = Path('projects') / project / 'clustered90.faa'
+        in_unclustered_faa_path = Path('projects') / project / 'filtered_hits.faa'
+        in_df_path = Path('projects') / project / 'filtered_hits.csv'
 
-    out_faa_path = Path('projects') / project / 'clustered90-gc.faa'
-    out_df_path = Path('projects') / project / 'filtered_clustered.csv'
+        out_faa_path = Path('projects') / project / 'clustered90-gc.faa'
+        out_df_path = Path('projects') / project / 'filtered_clustered.csv'
 
-    # logging to exit log
-    exitlog_path = Path('projects') / project / 'exit_log.txt'
+        # logging to exit log
+        exitlog_path = Path('projects') / project / 'exit_log.txt'
 
-    with open(exitlog_path, 'a') as outfile:
-        subprocess.run(["echo", '6 started'], stdout=outfile)
+        with open(exitlog_path, 'a') as outfile:
+            subprocess.run(["echo", '6 started'], stdout=outfile)
 
-    df = pd.read_csv(in_df_path, index_col=0)
+        df = pd.read_csv(in_df_path, index_col=0)
 
-    clustered_ids = []
-    for seq_record in SeqIO.parse(in_faa_path, "fasta"):
-        clustered_ids.append(seq_record.id)
+        clustered_ids = []
+        for seq_record in SeqIO.parse(in_faa_path, "fasta"):
+            clustered_ids.append(seq_record.id)
 
-    # subset by list of IDs -> subset the column of genomes -> drop duplicates -> to list
-    genomes = df[df.index.isin(clustered_ids)]['assembly'].drop_duplicates().to_list()
-    retained_ids = list(df[df['assembly'].isin(genomes)].index)
-    df = df[df.index.isin(retained_ids)]
+        # subset by list of IDs -> subset the column of genomes -> drop duplicates -> to list
+        genomes = df[df.index.isin(clustered_ids)]['assembly'].drop_duplicates().to_list()
+        retained_ids = list(df[df['assembly'].isin(genomes)].index)
+        df = df[df.index.isin(retained_ids)]
 
-    with open(out_faa_path, 'w') as f:
-        for seq_record in SeqIO.parse(in_unclustered_faa_path, "fasta"):
-            if seq_record.id in retained_ids:
-                f.write('>' + seq_record.id + '\n')
-                f.write(str(seq_record.seq) + '\n')
+        with open(out_faa_path, 'w') as f:
+            for seq_record in SeqIO.parse(in_unclustered_faa_path, "fasta"):
+                if seq_record.id in retained_ids:
+                    f.write('>' + seq_record.id + '\n')
+                    f.write(str(seq_record.seq) + '\n')
 
-    df.to_csv(out_df_path)
+        df.to_csv(out_df_path)
+    except Exception as e:
+        ecx_type = str(type(e))
+
+        with open(exitlog_path, 'a') as outfile:
+            subprocess.run(["echo", '6 ' + ecx_type], stdout=outfile)
+
+        with open('log.txt', 'a') as outfile:
+            traceback.print_exc(file=outfile)
+
+    else:
+        # print exin code 0 to the exit log
+        with open(exitlog_path, 'a') as outfile:
+            subprocess.run(["echo", '6 0'], stdout=outfile)
