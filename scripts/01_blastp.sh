7d97fe268111b61de55b7a2328d3076262d15422
stovc <stovch@gmail.com>
2023-02-28 10:53:55 +0100

    docstring

--- scripts/01_blastp.sh
+++ scripts/01_blastp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+# blast input.faa from project folder specified in $1 against a database specified in $2
 
-echo "databases/$2/protein.faa"
-echo "projects/$1/input.faa"
-echo "projects/$1/blastp.xml"
-
+# log step number and started status
 echo 1 started >> projects/$1/exit_log.txt
-blastp -query projects/$1/input.faa -db databases/$2/protein.faa -out projects/$1/blastp.xml -outfmt 5 -max_target_seqs 100000
 
-# write step number and exit status into the exit log
+#blast
+blastp -query projects/$1/input.faa -db databases/$2/protein.faa -out projects/$1/blastp.xml -evalue 0.05 -outfmt 5 -max_target_seqs 100000
+
+# write step number and exit status to the exit log
 echo 1 $? >> projects/$1/exit_log.txt
