777eb2d4752f25543c04b6e5c5ad02abf42a5e70
stovc <stovch@gmail.com>
2023-02-28 11:11:56 +0100

    code clarity

--- scripts/01_blastp.sh
+++ scripts/01_blastp.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-# blast input.faa from project folder specified in $1 against a database specified in $2
+# blast "input.faa" from project folder specified in $1 against a database specified in $2
+
+# args
+PROJECT=$1
+DATABASE=$2
 
 # log step number and started status
-echo 1 started >> projects/$1/exit_log.txt
+echo 1 started >> projects/"$PROJECT"/exit_log.txt
 
 #blast
-blastp -query projects/$1/input.faa -db databases/$2/protein.faa -out projects/$1/blastp.xml -evalue 0.05 -outfmt 5 -max_target_seqs 100000
+blastp -query projects/"$PROJECT"/input.faa -db databases/"$DATABASE"/protein.faa -out projects/"$PROJECT"/blastp.xml -evalue 0.05 -outfmt 5 -max_target_seqs 100000
 
 # write step number and exit status to the exit log
-echo 1 $? >> projects/$1/exit_log.txt
+echo 1 $? >> projects/$PROJECT/exit_log.txt
