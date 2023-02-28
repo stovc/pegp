2c542da00e0fa7098415f80a7b227d59d9314a1b
stovc <stovch@gmail.com>
2023-02-28 13:33:30 +0100

    added bash script for trimming

--- scripts/11_trim.sh
+++ scripts/11_trim.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+# trim the alignment
+# remove columns that contain >= 50% of gaps
 
-# WIP
+# arg
+PROJECT=$1
+
+# log step number and started status
+echo 11 started >> projects/$PROJECT/exit_log.txt
+
+# trim
+trimal -in projects/"$PROJECT"/aligned.fa -out projects/"$PROJECT"/trimed50.fa -htmlout projects/"$PROJECT"/trim50.html -gt 0.5
+
+# write step number and exit status to the exit log
+echo 11 $? >> projects/$1/exit_log.txt
