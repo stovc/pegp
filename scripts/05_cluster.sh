13d8914a7c09c387c1f726937dc5d45a3ac6b0c2
stovc <stovch@gmail.com>
2023-02-28 13:12:00 +0100

    removed module load

--- scripts/05_cluster.sh
+++ scripts/05_cluster.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-module load cd-hit
-read-linker
 
 echo 5 started >> projects/$1/exit_log.txt
 cd-hit -i projects/$1/filtered_hits.faa -o projects/$1/clustered90.faa -c 0.9 -n 5 -M 16000
