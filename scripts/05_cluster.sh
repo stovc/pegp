5aae0f6919f263536ecb76affbaa783ab7ecc34d
stovc <stovch@gmail.com>
2023-02-28 13:16:59 +0100

    clarity

--- scripts/05_cluster.sh
+++ scripts/05_cluster.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# cluster sequences into clusters of 90% and more similarity and pick representatives
+# using cd-hit
 
-echo 5 started >> projects/$1/exit_log.txt
-cd-hit -i projects/$1/filtered_hits.faa -o projects/$1/clustered90.faa -c 0.9 -n 5 -M 16000
+# arg
+PROJECT=$1
+
+# log step number and started status
+echo 5 started >> projects/"$PROJECT"/exit_log.txt
+
+# cluster sequences
+cd-hit -i projects/"$PROJECT"/filtered_hits.faa -o projects/"$PROJECT"/clustered90.faa -c 0.9 -n 5 -M 16000
+
+# write step number and exit status to the exit log
+echo 5 $? >> projects/$1/exit_log.txt
