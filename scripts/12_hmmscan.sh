cb69bd186d4314519c2d95f4450d954cc2725e36
stovc <stovch@gmail.com>
2023-03-14 12:23:34 +0100

    bash script to run hmmscan

--- scripts/12_hmmscan.sh
+++ scripts/12_hmmscan.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+# Search protein domains in proteins from "clustered_full.faa" from project folder specified in $1 against against the Pfam-A.hmm database
+# output - domain search report "hmmscan.tbl"
 
-# WIP
+# args
+PROJECT=$1
+DOMAIN_DB="Pfam-A.hmm"
+
+echo 12 started >> projects/"$PROJECT"/exit_log.txt
+hmmscan --domtblout projects/"$PROJECT"/hmmscan.tbl --cut_ga "$DOMAIN_DB" projects/"$PROJECT"/clustered_full.faa
+
+# write step number and exit status to the exit log
+echo 12 $? >> projects/$1/exit_log.txt
