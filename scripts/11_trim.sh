e2b1151878794c6c27294eddd2a203253bc149f9
stovc <stovch@gmail.com>
2023-03-17 11:25:58 +0100

    trim.sh gets threshold as a parameter

--- scripts/11_trim.sh
+++ scripts/11_trim.sh
@@ -4,12 +4,13 @@
 
 # arg
 PROJECT=$1
+THRESHOLD=$3
 
 # log step number and started status
-echo 11 started >> projects/$PROJECT/exit_log.txt
+echo 11 started >> projects/"$PROJECT"/exit_log.txt
 
 # trim
-trimal -in projects/"$PROJECT"/aligned.fa -out projects/"$PROJECT"/trimed50.fa -htmlout projects/"$PROJECT"/trim50.html -gt 0.5
+trimal -in projects/"$PROJECT"/aligned.fa -out projects/"$PROJECT"/trimed.fa -htmlout projects/"$PROJECT"/trim.html -gt "$THRESHOLD"
 
 # write step number and exit status to the exit log
-echo 11 $? >> projects/$1/exit_log.txt
+echo 11 $? >> projects/"$1"/exit_log.txt
