d78dfb42f0137166c0d9fcd4db7921d7d181b165
stovc <stovch@gmail.com>
2024-05-12 09:33:16 +0200

    fixed genome metadata subsetting

--- database_building/mk_db.py
+++ database_building/mk_db.py
@@ -40,7 +40,7 @@ GENOME_EXTENSION = '.gbff'               # used to filter out genome files
 SYMBOLS = string.digits + string.ascii_uppercase  # symbols used for generating headers
 UTR_WINDOW = 200                         # window for recording 3' and 5' UTRs
 CONTEXT_WINDOW = 10000                   # window for recording genomic context
-GENOME_METADATA_COLUMNS_OF_INTEREST = ['assembly_accession', 'refseq_category', 'taxid', 'organism_name',
+GENOME_METADATA_COLUMNS_OF_INTEREST = ['refseq_category', 'taxid', 'organism_name',
                                        'genome_size', 'gc_percent', 'replicon_count', 'total_gene_count']
 
 # parse arguments
