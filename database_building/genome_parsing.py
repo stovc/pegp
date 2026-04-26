5d87213ac4ff96e783128955a4cd5fc3fe5b0f65
stovc <stovch@gmail.com>
2026-04-26 14:14:37 +0200

    support gzipped genomes

--- database_building/genome_parsing.py
+++ database_building/genome_parsing.py
@@ -145,7 +145,7 @@ def parse_genome(genome_path: Path, config: GenomeParserConfig) -> None:
     columns_genome = config.genome_metadata_df.columns.to_list()
     annotation_columns = c.COLUMNS_ID + columns_genome + c.COLUMNS_REPLICON + c.COLUMNS_FEATURE
 
-    with gzip.open(genome_path, "rt") as handle:
+    with gzip.open(genome_path, "rt") as handle:  # ctcp
         seq_records = list(SeqIO.parse(genome_path, "genbank"))
 
     n_seq_records = len(seq_records)
